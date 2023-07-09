fca0d6e4baaf8eacc006048206b2fa2f491cd753
Aydin Karatas <alckaratas18@gmail.com>
2023-07-08 23:19:38 -0700

    added core limits for fasterq-dump and trim_galore

--- general_bash_functions/software_paths.sh
+++ general_bash_functions/software_paths.sh
@@ -5,7 +5,12 @@
 ###############################################################################
 #!/bin/bash
 
+prefetch="$HOME/software/sratoolkit.3.0.5-ubuntu64/bin/prefetch-orig.3.0.5"
 
 fasterq_dump="$HOME/software/sratoolkit.3.0.5-ubuntu64/bin/fasterq-dump-orig.3.0.5"
+fasterq_dump_cores=6
+
 trim_galore="$HOME/software/TrimGalore-0.6.10/trim_galore"
+trim_galore_cores=6
+
 megahit="$HOME/software/MEGAHIT-1.2.9-Linux-x86_64-static/bin/megahit"
