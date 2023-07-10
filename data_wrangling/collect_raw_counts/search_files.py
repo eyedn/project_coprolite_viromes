4d8402b04b9b3875acab0f41e6ec2e398ff24c89
Aydin Karatas <alckaratas18@gmail.com>
2023-07-10 14:05:07 -0700

    adjusted naming method

--- data_wrangling/collect_raw_counts/search_files.py
+++ data_wrangling/collect_raw_counts/search_files.py
@@ -14,10 +14,9 @@ def get_counts_dict(files: typing.List[typing.Union[str, typing.BinaryIO]]) \
     counts: typing.Dict[str, typing.Dict[str, int]] = {}
     for file in files:
         # format sample name
-        sample_name = file.strip().split("/")[-1]
-        sample_name = sample_name.strip().split(".")[0]
+        sample_name = file.strip().split("/")[-2]
         sample_name = sample_name.split("_")[:1]
-        sample_name = ''.join(sample_name)
+        sample_name = '_'.join(sample_name)
         # create counts dict for each sample; add it to the main counts dict
         file_annotations = read_gff.read_annotations(file)
         sample_counts = search_annot.get_counts(file_annotations)
