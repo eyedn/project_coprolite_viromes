ca94b310e6bc51acaaf53123cf87688b4ce64b13
Aydin Karatas <alckaratas18@gmail.com>
2023-07-27 04:26:25 -0700

    renamed main pipeline script

--- run_multi_step/main.sh
+++ run_multi_step/main.sh
@@ -17,5 +17,8 @@ num_cores=$3
 # call assembly script
 . megahit_assembly/assembly.sh "$origin" "$origin_parent" "$num_cores"
 
-# create lifestyle predictions of viral contigs
+# predict phage contigs and their lifestyle, family, and hosts
 . phatyp_predictions/phage_predictions.sh "$origin" "$origin_parent" "$num_cores"
+
+# annotate phages for bacterial genes
+. prokka_annotations/bacterial_annotation.sh "$origin" "$origin_parent" "$num_cores"
