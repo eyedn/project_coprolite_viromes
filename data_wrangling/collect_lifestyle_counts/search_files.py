0eb12b73ee1ea315ec77987387c335208cfc2c26
Aydin Karatas <alckaratas18@gmail.com>
2023-07-28 01:42:06 -0700

    adjusted indexing to get sample name

--- data_wrangling/collect_lifestyle_counts/search_files.py
+++ data_wrangling/collect_lifestyle_counts/search_files.py
@@ -12,8 +12,8 @@ def get_counts_dict(files: typing.List[typing.Union[str, typing.BinaryIO]]) \
     counts: typing.Dict[str, typing.Dict[str, int]] = {}
     for file in files:
         # format sample name
-        sample_name = file.strip().split("/")[-2]
-        sample_name = sample_name.split("_")[:-2]
+        sample_name = file.strip().split("/")[-3]
+        sample_name = sample_name.split("_")[:-1]
         sample_name = '_'.join(sample_name)
         # create counts dict for each sample; add it to the main counts dict
         sample_counts = get.get_lifestyle_counts(file)
