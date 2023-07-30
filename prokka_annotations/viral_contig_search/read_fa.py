c4ff3f20f7753c7490c7ce5ff08e4d85eea97230
Aydin Karatas <alckaratas18@gmail.com>
2023-07-30 00:47:31 -0700

    check if contigs already processed

--- prokka_annotations/viral_contig_search/read_fa.py
+++ prokka_annotations/viral_contig_search/read_fa.py
@@ -8,8 +8,9 @@ from . import contig
 
 
 # read an fa file of contigs and return a list of contigs of that fa file
-def read_contigs(fa_file: typing.TextIO) -> typing.List[contig.Contig]:
-    contigs: typing.List[contig.Contig] = []
+def read_contigs(fa_file: typing.TextIO, contigs: typing.List[contig.Contig] = []) \
+    -> typing.List[contig.Contig]:
+    contig_labels: typing.List[str] = []
     with open(fa_file, "r") as f:
         for line in f.readlines():
             curr_line = str(line.strip())
@@ -17,6 +18,11 @@ def read_contigs(fa_file: typing.TextIO) -> typing.List[contig.Contig]:
             if curr_line.startswith(">"):
                 line_data = curr_line.split()
                 contigs.append(contig.Contig(line_data))
+
+                # skip to next contig if it was already added
+                if line_data[0] in contig_labels:
+                    continue
+                contig_labels.append(line_data[0])
             # line without ">" contain sequence info related to the current label
             else:
                 contigs[-1].append_to_sequence(curr_line)
