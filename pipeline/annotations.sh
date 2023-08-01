dc8afc4387e37f1c04a300a5ca0c415ace35cae7
Aydin Karatas <alckaratas18@gmail.com>
2023-07-31 21:14:42 -0700

    added .sh to bacterial_annot_all

--- pipeline/annotations.sh
+++ pipeline/annotations.sh
@@ -15,7 +15,7 @@ num_cores=$3
 . prokka_annotations/viral_annotation.sh "$origin" "$origin_parent" "$num_cores"
 
 # annotate all contigs for bacterial genes
-. prokka_annotations/bacterial_annot_all "$origin" "$origin_parent" "$num_cores"
+. prokka_annotations/bacterial_annot_all.sh "$origin" "$origin_parent" "$num_cores"
 
 # create a new contigs file that contains only viral and/or phage contigs
 . prokka_annotations/create_viral_phage_contigs.sh "$origin" "$origin_parent" "$num_cores"
