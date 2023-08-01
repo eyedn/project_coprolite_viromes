32db98f4d9409175f3f7595d0f2bacdcfe22acf6
Aydin Karatas <alckaratas18@gmail.com>
2023-07-31 21:11:59 -0700

    temp. removed pal-BMS

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -8,10 +8,10 @@ cd $HOME/project_coprolite_viromes
 
 script=$1
 
-valid_ori="pal-AWC pal-BEL pal-BMS pal-ENG pal-ITA pal-PER pal-ZAF pal-ZAP \
+valid_ori="pal-AWC pal-BEL pal-ENG pal-ITA pal-PER pal-ZAF pal-ZAP \
             ind-DNK ind-ESP ind-USA \
             pre-FJI pre-MDG pre-MEX pre-PER pre-TZA"
 
 for ori in $valid_ori; do
-    ./submit_script.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o $ori
+    ./submit_script.sh -s $script -d 8 -c 16 -p $SCRATCH/project_coprolite_viromes -o $ori
 done
