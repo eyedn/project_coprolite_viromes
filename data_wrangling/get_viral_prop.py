3cfcb78af044dbcdedbbc8f070b09f72d2c98518
Aydin Karatas <alckaratas18@gmail.com>
2023-08-02 14:59:11 -0700

    corrected argv indeces

--- data_wrangling/get_viral_prop.py
+++ data_wrangling/get_viral_prop.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     # fa_list_file = "/Users/aydinkaratas/Documents/Research/project_coprolite_viromes/data_wrangling/fa_files.txt"
     # gff_list_file = "/Users/aydinkaratas/Documents/Research/project_coprolite_viromes/data_wrangling/gff_files.txt"
     # csv_path = "/Users/aydinkaratas/Documents/Research/project_coprolite_viromes/data_wrangling/text.csv"
-    fa_list_file = argv[1]
-    gff_list_file = argv[2]
-    csv_path = argv[3]
+    fa_list_file = argv[0]
+    gff_list_file = argv[1]
+    csv_path = argv[2]
     gff_list = create_list.create_list(gff_list_file)
     fa_list = create_list.create_list(fa_list_file)
     viral_gene_coverage = gvp.get.get_viral_prop_from_annot(fa_list, gff_list)
