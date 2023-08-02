f6b46fb6d144f2e598e231dd919db6d7f3c06a42
Aydin Karatas <alckaratas18@gmail.com>
2023-08-02 14:56:37 -0700

    updated to work on hoffman2

--- data_wrangling/get_viral_prop.sh
+++ data_wrangling/get_viral_prop.sh
@@ -17,31 +17,38 @@ project_dir=$2
 num_cores=$3
 
 # define directories and file
+contigs_dir="${project_dir}/contigs"
 prokka_annotations="${project_dir}/genome_annotation"
 data_dir="$project_dir/data"
-search_dir="$prokka_annotations/*annotation_phage"
+annot_search_dir="$prokka_annotations/*viral"
 gff_list="$data_dir/viral_prop_tmp.txt"
 csv_path="$data_dir/viral_prop.csv"
 
 # create a list of all files to generate counts from
+ls $contigs_dir/*/*phage_contigs.fa.gz > $fa_list
+echo "$(timestamp): get_viral_prop: generated file of all contig paths needed"
+echo "$(timestamp): get_viral_prop: using the following contig files:"
+cat $fa_list
+
 ls $search_dir/*gff.gz > $gff_list
-echo "$(timestamp): get_viral_prop: generated file of all file paths needed"
-echo "$(timestamp): get_viral_prop: using the following files:"
+echo "$(timestamp): get_viral_prop: generated file of all gff paths needed"
+echo "$(timestamp): get_viral_prop: using the following gff files:"
 cat $gff_list
 
 # generate counts with python script
 echo "===================================================================================================="
 echo "$(timestamp): get_viral_prop: generating bacterial gene counts"
 echo "===================================================================================================="
-python3 data_wrangling/collect_ec_counts.py \
+python3 data_wrangling/get_viral_prop.py \
+	$fa_list \
 	$gff_list \
 	$csv_path
 rm $gff_list
 
 # check if raw counts was created
 if ls $csv_path 1> /dev/null 2>&1; then
-	echo "$(timestamp): get_viral_prop: bacterial gene counts csv created"
+	echo "$(timestamp): get_viral_prop: viral prop csv created"
 else
-	echo "$(timestamp): get_viral_prop: bacterial gene counts csv not found"
+	echo "$(timestamp): get_viral_prop: viral prop csv not found"
 	exit 1
 fi
