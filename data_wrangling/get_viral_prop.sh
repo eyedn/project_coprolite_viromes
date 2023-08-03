45d448d771f60dee268c5090e8bcfc688850d9c6
Aydin Karatas <alckaratas18@gmail.com>
2023-08-02 22:30:54 -0700

    removed deletion of tmp files

--- data_wrangling/get_viral_prop.sh
+++ data_wrangling/get_viral_prop.sh
@@ -42,8 +42,6 @@ echo "==========================================================================
 echo "$(timestamp): get_viral_prop: generating viral prop. counts"
 echo "===================================================================================================="
 python3 data_wrangling/get_viral_prop.py $fa_list $gff_list $csv_path
-rm $fa_list
-rm $gff_list
 
 # check if raw counts was created
 if ls $csv_path 1> /dev/null 2>&1; then
