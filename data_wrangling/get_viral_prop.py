dba9cfdaa37f0b7e78242b99a3b69deeca036481
Aydin Karatas <alckaratas18@gmail.com>
2023-08-02 22:55:50 -0700

    adjusted argv indeces

--- data_wrangling/get_viral_prop.py
+++ data_wrangling/get_viral_prop.py
@@ -10,12 +10,10 @@ import get_viral_prop as gvp
 
 
 if __name__ == "__main__":
-    fa_list_file = argv[0]
-    gff_list_file = argv[1]
-    csv_path = argv[2]
+    fa_list_file = argv[1]
+    gff_list_file = argv[2]
+    csv_path = argv[3]
     fa_list = create_list.create_list(fa_list_file)
-    print(fa_list)
     gff_list = create_list.create_list(gff_list_file)
-    print(gff_list)
     viral_gene_coverage = gvp.get.get_viral_prop_from_annot(fa_list, gff_list)
     viral_gene_coverage_dict = gvp.create.create_df(viral_gene_coverage, csv_path)
