94649e7b175be58a4ddff6b1e87043e8934e2bbf
Aydin Karatas <alckaratas18@gmail.com>
2023-08-02 22:36:51 -0700

    print files list

--- data_wrangling/get_viral_prop.py
+++ data_wrangling/get_viral_prop.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
     gff_list_file = argv[1]
     csv_path = argv[2]
     fa_list = create_list.create_list(fa_list_file)
+    print('\n'.join(fa_list))
     gff_list = create_list.create_list(gff_list_file)
+    print('\n'.join(gff_list))
     viral_gene_coverage = gvp.get.get_viral_prop_from_annot(fa_list, gff_list)
     viral_gene_coverage_dict = gvp.create.create_df(viral_gene_coverage, csv_path)
