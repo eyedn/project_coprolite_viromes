a57544960d6e69423bf9cbe8404801bacb858002
Aydin Karatas <alckaratas18@gmail.com>
2023-08-06 01:48:54 -0700

    renamed sralite-based fastq file for simplicity

--- download/bash_functions/quality_control.sh
+++ download/bash_functions/quality_control.sh
@@ -5,7 +5,7 @@
 ###############################################################################
 
 
-# use trim galore to remove adapters and low quality reads
+# use trim galore to remove adapters and trim reads for quality
 quality_control() {
 	local id=$1
 	local fastq_raw_dir=$2
@@ -25,39 +25,48 @@ quality_control() {
 		cores_to_use="$num_cores"
 	fi
 
+	# rename sralite fastq files for simplicity
+	if [ -f "${fastq_raw_dir}/${id}/${id}.sralite_1.fastq.gz" ] && \
+	[ -f "${fastq_raw_dir}/${id}/${id}.sralite_2.fastq.gz" ]; then
+		mv ${fastq_raw_dir}/${id}/${id}.sralite_1.fastq.gz \
+			${fastq_raw_dir}/${id}/${id}_1.fastq.gz
+		mv ${fastq_raw_dir}/${id}/${id}.sralite_2.fastq.gz \
+			${fastq_raw_dir}/${id}/${id}_2.fastq.gz
+	elif [ -f "${fastq_raw_dir}/${id}/${id}.sralite.fastq.gz" ]; then
+		mv ${fastq_raw_dir}/${id}/${id}.sralite.fastq.gz \
+			${fastq_raw_dir}/${id}/${id}.fastq.gz
+	fi
+
 	# trim adaptors and low quality positions; remove low quality reads
-	# run quality check based on if sample is single-end or paired-end data
+	# remove human reads
 	mkdir -p $fastq_trimmed_dir
 	if [ -f "${fastq_raw_dir}/${id}/${id}_1.fastq.gz" ] && \
 	[ -f "${fastq_raw_dir}/${id}/${id}_2.fastq.gz" ]; then
 		$trim_galore \
 			--paired \
 			-o ${fastq_trimmed_dir}/${id} \
+			--basename ${id} \
 			--gzip \
 			-j $cores_to_use \
 			${fastq_raw_dir}/${id}/${id}_1.fastq.gz \
 			${fastq_raw_dir}/${id}/${id}_2.fastq.gz
-	elif [ -f "${fastq_raw_dir}/${id}/${id}.sralite_1.fastq.gz" ] && \
-	[ -f "${fastq_raw_dir}/${id}/${id}.sralite_2.fastq.gz" ]; then
-		$trim_galore \
-			--paired \
-			-o ${fastq_trimmed_dir}/${id} \
-			--gzip \
-			-j $cores_to_use \
-			${fastq_raw_dir}/${id}/${id}.sralite_1.fastq.gz \
-			${fastq_raw_dir}/${id}/${id}.sralite_2.fastq.gz
+
+		# TODO: integrate host removal for paired data
+		$bowtie2 -p 8 -x host_DB -1 SAMPLE_R1.fastq.gz -2 SAMPLE_R2.fastq.gz -S SAMPLE_mapped_and_unmapped.sam
+		$samtools view -bS SAMPLE_mapped_and_unmapped.sam > SAMPLE_mapped_and_unmapped.bam
+		$samtools view -b -f 12 -F 256 SAMPLE_mapped_and_unmapped.bam > SAMPLE_bothReadsUnmapped.bam 
+		$samtools sort -n -m 5G -@ 2 SAMPLE_bothReadsUnmapped.bam -o SAMPLE_bothReadsUnmapped_sorted.bam
+		$samtools fastq -@ 8 SAMPLE_bothReadsUnmapped_sorted.bam \
+			-1 SAMPLE_host_removed_R1.fastq.gz \
+			-2 SAMPLE_host_removed_R2.fastq.gz \
+			-0 /dev/null -s /dev/null -n
 	elif [ -f "${fastq_raw_dir}/${id}/${id}.fastq.gz" ]; then
 		$trim_galore \
 			-o ${fastq_trimmed_dir}/${id} \
+			--basename ${id} \
 			--gzip \
 			-j $cores_to_use \
 			${fastq_raw_dir}/${id}/${id}.fastq.gz
-	elif [ -f "${fastq_raw_dir}/${id}/${id}.sralite.fastq.gz" ]; then
-		$trim_galore \
-			-o ${fastq_trimmed_dir}/${id} \
-			--gzip \
-			-j $cores_to_use \
-			${fastq_raw_dir}/${id}/${id}.sralite.fastq.gz
 	fi
 
 	# check if trimmd file(s) created
