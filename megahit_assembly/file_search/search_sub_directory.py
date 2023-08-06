031840466f96c80419008cedadf8d66ca7431117
Aydin Karatas <alckaratas18@gmail.com>
2023-08-06 01:50:18 -0700

    updated fastq file endings

--- megahit_assembly/file_search/search_sub_directory.py
+++ megahit_assembly/file_search/search_sub_directory.py
@@ -19,10 +19,10 @@ def get_file_names(fastq_dir: typing.Union[str, os.PathLike]) \
         if str(file).endswith("fq.gz"):
             file_path = os.path.join(fastq_dir, file)
             # type of data can be identified by what the file name ends in
-            if str(file).endswith("trimmed.fq.gz"):
+            if str(file).endswith("R.fastq.gz"):
                 my_single_data = file_path
-            elif str(file).endswith("1.fq.gz"):
+            elif str(file).endswith("R1.fastq.gz"):
                 my_paired_data_1 = file_path
-            elif str(file).endswith("2.fq.gz"):
+            elif str(file).endswith("R2.fastq.gz"):
                 my_paired_data_2 = file_path
     return my_single_data, my_paired_data_1, my_paired_data_2
