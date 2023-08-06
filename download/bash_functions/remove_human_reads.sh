5c0a338ee61b5c6095b89c0ffbe7d6d07cac3afa
Aydin Karatas <alckaratas18@gmail.com>
2023-08-06 01:59:03 -0700

    added section for unpaired reads

--- download/bash_functions/remove_human_reads.sh
+++ download/bash_functions/remove_human_reads.sh
@@ -24,27 +24,48 @@ remove_human_reads() {
 	if [ -f "${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz" ] && \
 	[ -f "${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz" ]; then
 		# TODO: test pipeline with paired data
-		$bowtie2 -p ${num_cores} -x $hum_genome_ref \
-			-1 SAMPLE_R1.fastq.gz \
-			-2 SAMPLE_R2.fastq.gz \
+		$bowtie2 -p ${num_cores} -x ${hum_genome_ref} \
+			-1 ${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz \
+			-2 ${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz \
 			-S ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam
+
 		$samtools view -bS ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam \
 			> ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam
+
 		$samtools view -b -f 12 -F 256 \
 			${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam \
 			> ${fastq_clean_dir}/${id}/${id}_unmapped_paired_tmp.bam 
-		$samtools sort -n -m 8G -@ 2 \
+
+		$samtools sort -n -m ${samtools_m}G -@ 2 \
 			${fastq_clean_dir}/${id}/${id}_unmapped_paired_tmp.bam  \
 			-o ${fastq_clean_dir}/${id}/${id}_unmapped_paired_sorted_tmp.bam
+
 		$samtools fastq -@ 8 \
 			${fastq_clean_dir}/${id}/${id}_unmapped_paired_sorted_tmp.bam \
 			-1 ${fastq_clean_dir}/${id}/${id}_R1.fastq.gz \
 			-2 ${fastq_clean_dir}/${id}/${id}_R2.fastq.gz \
 			-0 /dev/null -s /dev/null -n
-		gzip ${fastq_clean_dir}/${id}/*R*fastq.gz
 	elif [ -f "${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz" ]; then
-		# TODO: integrate host removal for unpaired data
 		# TODO: test pipeline with unpaired data
+		bowtie2 -p ${num_cores} -x ${hum_genome_ref} \
+			-U ${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz \
+			-S ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam
+
+		samtools view -bS ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam \
+			> ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam
+
+		samtools view -b -f 4 \
+			${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam \
+			> ${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_tmp.bam
+
+		samtools sort -n -m ${samtools_m}G -@ 2 \
+			${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_tmp.bam  \
+			-o ${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_sorted_tmp.bam
+
+		samtools fastq -@ 8 \
+			$${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_sorted_tmp.bam \
+			-1 ${fastq_clean_dir}/${id}/${id}_RU.fastq.gz \
+			-0 /dev/null -s /dev/null -n
 	else
 		echo "$(timestamp): quality_check_fastq: ERROR! trimmed fastq files not found"
 		exit 1
@@ -53,9 +74,8 @@ remove_human_reads() {
 	# check if cleaned file(s) created
 	if ls ${fastq_clean_dir}/${id}/*R*fastq.gz 1> /dev/null 2>&1; then
 		echo "$(timestamp): quality_check_fastq: clean fastq files created"
-		gzip ${fastq_clean_dir}/${id}/*R*fastq.gz
 		rm ${fastq_clean_dir}/${id}/*tmp*
-		rm -r ${fastq_trimmed_dir}/${id}
+		rm ${fastq_trimmed_dir}/${id}/*fq.gz
 	else
 		echo "$(timestamp): quality_check_fastq: ERROR! clean fastq files not found"
 		exit 1
