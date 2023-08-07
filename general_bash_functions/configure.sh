5384dd26198fe96c111806c27deda5257d8fba0f
Aydin Karatas <alckaratas18@gmail.com>
2023-08-07 01:32:41 -0700

    added reference and trf directory paths

--- general_bash_functions/configure.sh
+++ general_bash_functions/configure.sh
@@ -11,6 +11,10 @@ hum_genome_ref="$HOME/references/GRCh38_noalt_as/GRCh38_noalt_as"
 
 phabox="$HOME/software/PhaBOX"
 
+ref_db="$HOME/references"
+
+trf_dir="$HOME/.conda/pkgs/trf-4.09.1-h031d066_4/bin"
+
 samtools_m=32
 
 fasterq_dump_cores=6
