03a2de11d01d8dfe30345e2784ad34b9662c2fb4
Aydin Karatas <alckaratas18@gmail.com>
2023-08-07 02:34:02 -0700

    renamed predictions dir to phabox_predictions

--- pipeline_by_origin/3_predict_viral_character.sh
+++ pipeline_by_origin/3_predict_viral_character.sh
@@ -12,7 +12,7 @@ origin_parent=$2
 num_cores=$3 
 
 # predict phage contigs and their lifestyle, family, and hosts
-. phatyp_predictions/phage_predictions.sh "$origin" "$origin_parent" "$num_cores"
+. phabox_predictions/phage_predictions.sh "$origin" "$origin_parent" "$num_cores"
 
 # annotate phages for bacterial genes
 . prokka_annotations/bacterial_annot_phamer.sh "$origin" "$origin_parent" "$num_cores"
