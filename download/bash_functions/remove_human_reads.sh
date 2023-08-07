6923e58f924892ba5026e5073be0d002ac169e6f
Aydin Karatas <alckaratas18@gmail.com>
2023-08-07 02:34:21 -0700

    added sections for post paired and unpaired reads

--- download/bash_functions/remove_human_reads.sh
+++ download/bash_functions/remove_human_reads.sh
@@ -5,7 +5,7 @@
 ###############################################################################
 
 
-# use bowtie and samtools to remove human sample
+# use kneaddata to remove human reads
 remove_human_reads() {
 	local id=$1
 	local fastq_trimmed_dir=$2
@@ -20,62 +20,47 @@ remove_human_reads() {
 
 	# remove host sequences based on if data is paired or unpaired
 	mkdir -p $fastq_clean_dir
-	mkdir -p ${fastq_clean_dir}/${id}
 	if [ -f "${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz" ] && \
 	[ -f "${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz" ]; then
-		# TODO: test pipeline with paired data
-		$bowtie2 -p ${num_cores} -x ${hum_genome_ref} \
-			-1 ${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz \
-			-2 ${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz \
-			-S ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam
+		kneaddata \
+			--input1 ${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz \
+			--input2 ${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz \
+			--reference-db $ref_db \
+			--output ${fastq_clean_dir}/${id} \
+			--trf $trf_dir \
+			--sequencer-source none \
+			--threads $num_cores
 
-		$samtools view -bS ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam \
-			> ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam
-
-		$samtools view -b -f 12 -F 256 \
-			${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam \
-			> ${fastq_clean_dir}/${id}/${id}_unmapped_paired_tmp.bam 
-
-		$samtools sort -n -m ${samtools_m}G -@ 2 \
-			${fastq_clean_dir}/${id}/${id}_unmapped_paired_tmp.bam  \
-			-o ${fastq_clean_dir}/${id}/${id}_unmapped_paired_sorted_tmp.bam
-
-		$samtools fastq -@ 8 \
-			${fastq_clean_dir}/${id}/${id}_unmapped_paired_sorted_tmp.bam \
-			-1 ${fastq_clean_dir}/${id}/${id}_R1.fastq.gz \
-			-2 ${fastq_clean_dir}/${id}/${id}_R2.fastq.gz \
-			-0 /dev/null -s /dev/null -n
+		mv ${fastq_clean_dir}/${id}/${id}_val_1_kneaddata_paired_1.fastq \
+			> ${fastq_clean_dir}/${id}/${id}_R1.fastq
+		mv ${fastq_clean_dir}/${id}/${id}_val_1_kneaddata_paired_2.fastq \
+			> ${fastq_clean_dir}/${id}/${id}_R2.fastq
+		mv ${fastq_clean_dir}/${id}/${id}_val_1_kneaddata.log \
+			> ${fastq_clean_dir}/${id}/${id}.log 
 	elif [ -f "${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz" ]; then
-		# TODO: test pipeline with unpaired data
-		bowtie2 -p ${num_cores} -x ${hum_genome_ref} \
-			-U ${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz \
-			-S ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam
+		kneaddata \
+			--unpaired ${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz \
+			--reference-db $ref_db \
+			--output ${fastq_clean_dir}/${id} \
+			--trf $trf_dir \
+			--sequencer-source none \
+			--threads $num_cores
 
-		samtools view -bS ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.sam \
-			> ${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam
-
-		samtools view -b -f 4 \
-			${fastq_clean_dir}/${id}/${id}_mapped_and_unmapped_tmp.bam \
-			> ${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_tmp.bam
-
-		samtools sort -n -m ${samtools_m}G -@ 2 \
-			${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_tmp.bam  \
-			-o ${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_sorted_tmp.bam
-
-		samtools fastq -@ 8 \
-			$${fastq_clean_dir}/${id}/${id}_unmapped_unpaired_sorted_tmp.bam \
-			-1 ${fastq_clean_dir}/${id}/${id}_RU.fastq.gz \
-			-0 /dev/null -s /dev/null -n
+		mv ${fastq_clean_dir}/${id}/${id}_trimmed_kneaddata.fastq \
+			> ${fastq_clean_dir}/${id}/${id}_RU.fastq
+		mv ${fastq_clean_dir}/${id}/${id}_trimmed_kneaddata.log \
+			> ${fastq_clean_dir}/${id}/${id}.log 
 	else
 		echo "$(timestamp): quality_check_fastq: ERROR! trimmed fastq files not found"
 		exit 1
 	fi
 
 	# check if cleaned file(s) created
-	if ls ${fastq_clean_dir}/${id}/*R*fastq.gz 1> /dev/null 2>&1; then
+	if ls ${fastq_clean_dir}/${id}/*R*fastq 1> /dev/null 2>&1; then
 		echo "$(timestamp): quality_check_fastq: clean fastq files created"
-		rm ${fastq_clean_dir}/${id}/*tmp*
 		rm ${fastq_trimmed_dir}/${id}/*fq.gz
+		rm ${fastq_clean_dir}/${id}/*kneaddata*
+		gzip ${fastq_clean_dir}/${id}/*R*fastq
 	else
 		echo "$(timestamp): quality_check_fastq: ERROR! clean fastq files not found"
 		exit 1
