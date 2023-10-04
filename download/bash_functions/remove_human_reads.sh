9b3b939fba2b09a889a3d9611fa098ad03227f8c
Aydin Karatas <alckaratas18@gmail.com>
2023-10-03 22:25:22 -0700

    provided arguments to --un-gz and --un-conc-gz

--- download/bash_functions/remove_human_reads.sh
+++ download/bash_functions/remove_human_reads.sh
@@ -20,9 +20,7 @@ remove_human_reads() {
 	fi
 
 	# remove host sequences based on if data is paired or unpaired
-	mkdir -p $fastq_clean_dir
-	orig_dir=$(pwd)
-	cd ${fastq_trimmed_dir}/${id}
+	mkdir -p ${fastq_clean_dir}/${id}
 	if [ -f "${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz" ] && \
 	[ -f "${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz" ]; then
 		$bowtie2 \
@@ -30,44 +28,28 @@ remove_human_reads() {
 			-x $hum_genome_ref \
 			-1 ${fastq_trimmed_dir}/${id}/${id}_val_1.fq.gz \
 			-2 ${fastq_trimmed_dir}/${id}/${id}_val_2.fq.gz \
-			--un-conc-gz \
-			${id}_SAMPLE_host_removed \
+			--un-conc-gz ${fastq_clean_dir}/${id} \
 			> ${id}_SAMPLE_mapped_and_unmapped.sam
-
-		# check if bowtie2 ran correctly
-		if [ $? -ne 0 ]; then
-			echo "$(timestamp): Error running bowtie2 for sample $id"
-			exit 1
-		fi
-
-		echo "$(timestamp): bowtie2 results"
-		ls
-
-		mv ${id}_SAMPLE_host_removed.1 \
-			${fastq_clean_dir}/${id}/${id}_R1.fastq.gz
-		mv ${id}_SAMPLE_host_removed.2 \
-			${fastq_clean_dir}/${id}/${id}_R2.fastq.gz
-		rm ${id}_SAMPLE_mapped_and_unmapped.sam
 	elif [ -f "${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz" ]; then
 		$bowtie2 \
 			-p $num_cores \
 			-x $hum_genome_ref \
 			-U ${fastq_trimmed_dir}/${id}/${id}_trimmed.fq.gz \
-			--un-gz \
-			${id}_SAMPLE_host_removed \
+			--un-gz ${fastq_clean_dir}/${id} \
 			> ${id}_SAMPLE_mapped_and_unmapped.sam
-
-		echo "$(timestamp): bowtie2 results"
-		ls
-
-		mv ${id}_SAMPLE_host_removed \
-			${fastq_clean_dir}/${id}/${id}_RU.fastq.gz
-		rm ${id}_SAMPLE_mapped_and_unmapped.sam
 	else
 		echo "$(timestamp): remove_human_reads: ERROR! trimmed fastq files not found"
 		exit 1
 	fi
-	cd $orig_dir
+
+	# check if bowtie2 ran correctly
+	if [ $? -ne 0 ]; then
+		echo "$(timestamp): Error running bowtie2 for sample $id"
+		exit 1
+	else
+		echo "$(timestamp): bowtie2 results"
+		ls ${fastq_clean_dir}/${id}
+	fi
 
 	# check if cleaned file(s) created
 	if ls ${fastq_clean_dir}/${id}/*R*fastq.gz 1> /dev/null 2>&1; then
