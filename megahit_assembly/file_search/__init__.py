f9908804b413b122123f7cb9f9d6181a4a15fb9d
Aydin Karatas <alckaratas18@gmail.com>
2023-10-05 21:46:26 -0700

    corrected import

--- megahit_assembly/file_search/__init__.py
+++ megahit_assembly/file_search/__init__.py
@@ -1,5 +1,3 @@
 from . import search_sample as search
 from . import search_acc_id
 from . import format_output as format
-from search_sample import search_sample
-from format_output import format_output
