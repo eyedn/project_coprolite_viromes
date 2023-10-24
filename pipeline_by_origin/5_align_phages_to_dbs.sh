d735667b88f95162cf2cd2dfb29054385d980546
Aydin Karatas <alckaratas18@gmail.com>
2023-10-24 11:24:07 -0700

    create pipeline script for hmm aligment

---
+++ pipeline_by_origin/5_align_phages_to_dbs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###############################################################################
+#       Aydin Karatas
+#		Project Coprolite Viromes
+#		4_annotate_viruses_for_metabolic.sh 
+###############################################################################
+cd $HOME/project_coprolite_viromes
+
+
+origin=$1
+origin_parent=$2
+num_cores=$3 
+
+# build hmm files for alignment
+. hmm_alignment/build_hmm_file.sh "$origin" "$origin_parent" "$num_cores"
+
+# align phages to vfdb
+. hmm_alignment/vf_alignment.sh "$origin" "$origin_parent" "$num_cores"
