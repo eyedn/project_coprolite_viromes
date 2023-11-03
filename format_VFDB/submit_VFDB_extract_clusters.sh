fff10245341739349c6fcd22b596c4057c74e7b7
Aydin Karatas <alckaratas18@gmail.com>
2023-11-03 00:41:37 -0700

    gave execute priv

--- format_VFDB/submit_VFDB_extract_clusters.sh
+++ format_VFDB/submit_VFDB_extract_clusters.sh
@@ -3,6 +3,7 @@ source /u/local/Modules/default/init/modules.sh
 module load python
 
 
+# run python script to create separate fasta files for each cluster
 python3 VFDB_extract_clusters.py \
     $SCRATCH/reference_fas/VFDB_setB_pro.fas \
     $SCRATCH/VFDB_cluster_profiles \
