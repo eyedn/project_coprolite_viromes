3c76bd8234828ccfd477aa7c14b5c622bdba907c
Aydin Karatas <alckaratas18@gmail.com>
2023-11-03 01:43:17 -0700

    made vfdb path a an inputted var

--- format_VFDB/submit_VFDB_extract_clusters.sh
+++ format_VFDB/submit_VFDB_extract_clusters.sh
@@ -4,7 +4,8 @@ module load python
 
 
 # run python script to create separate fasta files for each cluster
+vfdb=$1
 python3 VFDB_extract_clusters.py \
-    $SCRATCH/references/VFDB_setB_pro.fas \
+    $vfdb \
     $SCRATCH/VFDB_cluster_profiles \
     $SCRATCH/VFDB_cluster_profiles/clusters.txt
