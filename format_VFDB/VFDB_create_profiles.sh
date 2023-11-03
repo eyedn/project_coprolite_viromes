09d96e07c1d6a27684f9b9369e995eb57cb2367f
Aydin Karatas <alckaratas18@gmail.com>
2023-11-03 00:35:58 -0700

    corrected syntax of creating hmm file name

--- format_VFDB/VFDB_create_profiles.sh
+++ format_VFDB/VFDB_create_profiles.sh
@@ -7,25 +7,17 @@ conda activate msa
 # define number of cores to use arguments
 cores=$1
 
-# define directories for the output files and input fasta
-PROTEIN_DIR="$SCRATCH/VFDB_proteins"
-ALIGN_DIR="$SCRATCH/VFDB_align"
-PROFILE_DIR="$SCRATCH/VFDB_profiles"
-FILE=$(head -n ${SGE_TASK_ID} $SCRATCH/VFDB_clusters/clusters.txt | tail -n 1)
-
-# create directories if they don't exist
-mkdir -p "$PROTEIN_DIR"
-mkdir -p "$ALIGN_DIR"
-mkdir -p "$PROFILE_DIR"
+# input fasta
+FAS=$(head -n ${SGE_TASK_ID} $SCRATCH/VFDB_clusters/clusters.txt | tail -n 1)
 
 # run mafft on fasta file
-ALN=$(echo $FILE | sed 's/.fasta/.aln/')
-mafft --auto --thread $cores $FILE > $ALN
+ALN=$(echo $FAS | sed 's/.fasta/.aln/')
+mafft --auto --thread $cores $FAS > $ALN
 
 # Run hmmbuild on aligned fasta file to produce profile
 software="$HOME/software"
 hmmbuild="$software/hmmer-3.4/src/hmmbuild"
-HMM=$(echo $FILE | sed 's/.aln/.hmm/')
+ALN=$(echo $FILE | sed 's/.aln/.hmm/')
 $hmmbuild --amino --cpu $cores $HMM $ALN
 
 # check if hmm file was created and has content
