4433d5a3c71bcbd3133bdc7cf6b6d368fc84c83f
Aydin Karatas <alckaratas18@gmail.com>
2023-11-02 21:25:56 -0700

    corrected extraction of labels

--- format_VFDB/VFDB_extract_clusters.py
+++ format_VFDB/VFDB_extract_clusters.py
@@ -1,4 +1,5 @@
 import os
+import typing
 
 
 def split_clusters(cluster_file, sequence_file, output_dir):
@@ -7,20 +8,21 @@ def split_clusters(cluster_file, sequence_file, output_dir):
         cluster_id = ""
         for line in clstr_file:
             if line.startswith(">"):
-                cluster_id = line.strip().replace(">", "")
+                cluster_id = line.strip().split()[0]
                 clusters[cluster_id] = []
             else:
-                sequence_id = line.split(">")[1].split("...")[0]
+                sequence_id = line.split(">")[1].split("(")[0]
                 clusters[cluster_id].append(sequence_id.strip())
 
     sequences = {}
     with open(sequence_file, 'r') as seq_file:
         for line in seq_file:
             if line.startswith(">"):
-                seq_id = line.strip().replace(">", "")
-                sequences[seq_id] = line
+                seq_id = line.strip().replace(">", "").split("(")[0]
+                sequences[seq_id]: typing.List[str, str] = [None, None]
+                sequences[seq_id][0] = line
             else:
-                sequences[seq_id] += line
+                sequences[seq_id][1] += line
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -28,7 +30,8 @@ def split_clusters(cluster_file, sequence_file, output_dir):
     for cluster_id, members in clusters.items():
         with open(os.path.join(output_dir, f"cluster_{cluster_id}.fasta"), 'w') as out_file:
             for seq_id in members:
-                out_file.write(sequences[seq_id])
+                out_file.write(sequences[seq_id][0])
+                out_file.write(sequences[seq_id][1])
 
 if __name__ == "__main__":
     import sys
