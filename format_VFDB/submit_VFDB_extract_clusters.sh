c4a39acba6a28d988f5081dbfbe82eaaf8259393
Aydin Karatas <alckaratas18@gmail.com>
2023-11-03 00:51:51 -0700

    renamed to reference_fas to references

--- format_VFDB/submit_VFDB_extract_clusters.sh
+++ format_VFDB/submit_VFDB_extract_clusters.sh
@@ -5,6 +5,6 @@ module load python
 
 # run python script to create separate fasta files for each cluster
 python3 VFDB_extract_clusters.py \
-    $SCRATCH/reference_fas/VFDB_setB_pro.fas \
+    $SCRATCH/references/VFDB_setB_pro.fas \
     $SCRATCH/VFDB_cluster_profiles \
     $SCRATCH/VFDB_cluster_profiles/clusters.txt
