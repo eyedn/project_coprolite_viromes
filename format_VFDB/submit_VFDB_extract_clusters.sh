6651fc71ac4eeb5be9def5d528482516360159f4
Aydin Karatas <alckaratas18@gmail.com>
2023-11-03 01:45:24 -0700

    changed name of vfdb file

--- format_VFDB/submit_VFDB_extract_clusters.sh
+++ format_VFDB/submit_VFDB_extract_clusters.sh
@@ -4,8 +4,7 @@ module load python
 
 
 # run python script to create separate fasta files for each cluster
-vfdb=$1
 python3 VFDB_extract_clusters.py \
-    $vfdb \
+    $SCRATCH/references/VFDB.fas \
     $SCRATCH/VFDB_cluster_profiles \
     $SCRATCH/VFDB_cluster_profiles/clusters.txt
