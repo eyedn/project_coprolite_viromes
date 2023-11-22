5ec86c4b3365f6720393b16a630170e1dc415509
Aydin Karatas <alckaratas18@gmail.com>
2023-11-22 14:42:07 -0800

    update contigs list after reading phages if needed

--- prokka_annotations/identify_viral_phage_contigs.py
+++ prokka_annotations/identify_viral_phage_contigs.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     phage_fa = argv[2]
     gff_file = argv[3]
     my_contigs, my_contig_labels, my_contig_labels = vcs.read_fa.read_contigs(all_contigs_fa)
-    _, _, my_phage_labels = vcs.read_fa.read_contigs(phage_fa, my_contigs, my_contig_labels)
+    my_contigs, _, my_phage_labels = vcs.read_fa.read_contigs(phage_fa, my_contigs, my_contig_labels)
     my_viral_genes = vcs.read_gff.read_annotations(gff_file)
     vcs.results.print_annotated_contigs(my_contigs, my_viral_genes, my_contig_labels, my_phage_labels)
