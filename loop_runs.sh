1adb6250937c7fa5eae52a84ed3b41f35b7b4934
Aydin Karatas <alckaratas18@gmail.com>
2023-11-27 14:40:45 -0800

    removed loop; all runs hard coded

--- loop_runs.sh
+++ loop_runs.sh
@@ -8,10 +8,22 @@ cd $HOME/project_coprolite_viromes
 
 script=$1
 
-valid_ori="pal-AWC pal-BEL pal-BMS pal-PER pal-ZAF pal-ZAP \
-            ind-DNK ind-ESP ind-USA \
-            pre-FJI pre-MDG pre-MEX pre-PER pre-TZA"
+# run pal samples
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-AWC
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-BEL
+./run.sh -s $script -d 8 -c 16 -p $SCRATCH/project_coprolite_viromes -o pal-BMS
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ENG
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ZAF
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ZAP
 
-for ori in $valid_ori; do
-    ./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o $ori
-done
+# run ind samples
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-DNK
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-ESP
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-USA
+
+# run non-ind samples
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-FJI
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-MDG
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-MEX
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-PER
+./run.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-TZA
