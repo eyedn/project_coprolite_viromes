9a38394b0bab22e642e6898a7ab9135511e7cd68
Aydin Karatas <alckaratas18@gmail.com>
2025-07-31 23:48:09 -0700

    revert

--- run_ori.sh
+++ run_ori.sh
@@ -1,56 +1,60 @@
 #!/bin/bash
 ###############################################################################
 #     Aydin Karatas
-#     Project Coprolite Viromes
-#     run_ori.sh 
+#		  Project Coprolite Viromes
+#		  run_ori.sh 
 ###############################################################################
 cd $HOME/project_coprolite_viromes
 for FILE in general_bash_functions/* ; do source $FILE ; done
 
-# valid origin labels
+
+# paleo samples labeled as pal-{location} :
+#       pal-AWC pal-BEL pal-BMS pal-ENG pal-ZAF pal-ZAP
+# industrial samples labeled as ind-{location} :
+#       ind-DNK ind-ESP ind-USA
+# pre-industrial samples labeled as pre-{location} :
+#       pre-FJI pre-MDG pre-MEX pre-PER pre-TZA
 valid_ori=( pal-AWC pal-BEL pal-BMS pal-ENG pal-ZAF pal-ZAP \
             ind-DNK ind-ESP ind-USA \
             pre-FJI pre-MDG pre-MEX pre-PER pre-TZA \
             test all )
-
-# parse command line arguments
-while getopts "s:o:p:d:c:h:" opt; do
+            
+# read in arguments by flag and assign them to variables
+while getopts "s:o:p:d:c:" opt; do
   case $opt in
     s) script_name="$OPTARG";;
     o) origin="$OPTARG";;
     p) project_dir="$OPTARG";;
     d) data_per_core="$OPTARG";;
     c) cores="$OPTARG";;
-    h) hold_jid="$OPTARG";;
     *) echo "Unknown option: -$opt" >&2; exit 1;;
   esac
 done
 
-# check that all required arguments were provided
+# check that all arguements were provided
 args=("script_name" "origin" "project_dir" "data_per_core" "cores")
 for arg in "${args[@]}"; do
+  # if an argument is empty, it may have not been provided to the script
   if [ -z "${!arg}" ]; then
     echo "ERROR: missing <$arg>."
-    echo "Usage: ./run_ori.sh -s <script_name> -o <origin> -p <project_dir> -d <data_per_core> -c <cores> [-h <hold_jid>]"
+    echo "Usage Instructions: ./run_ori.sh -s <script_name> -o <origin> -p <project_dir> -d <data_per_core> -c <cores>"
     exit 1
+  # check orgin name to be in the list of valid origins
   elif [ "$arg" == "origin" ]; then
     check_args "${valid_ori[*]}" "$origin"
   fi
 done
 
-# determine number of tasks
+# run the submission command on hoffman2
 if [ "$origin" == "all" ]; then
   total_samples=1
 else
-  declare -i total_samples=$(wc -l < ${project_dir}/samples/${origin}_samples.txt)
+  declare -i total_samples=$(wc -l < ${project_dir}/samples/${origin}_samples.txt) 
 fi
 
 job_name=$(echo $script_name | cut -d '/' -f 2-)
 joblogs_output="$SCRATCH/joblogs/$origin/"
-
-# submit job
-if [ -z "${hold_jid:-}" ]; then
-  jobid=$(qsub \
+qsub \
     -cwd \
     -N "${origin}.${job_name}" \
     -o $joblogs_output \
@@ -59,24 +63,5 @@ if [ -z "${hold_jid:-}" ]; then
     -pe shared ${cores} \
     -M $USER@mail \
     -m ea \
-    -terse \
-    -t 1-${total_samples}%8 \
-    $script_name "$origin" "$project_dir" "$cores")
-else
-  jobid=$(qsub \
-    -cwd \
-    -N "${origin}.${job_name}" \
-    -o $joblogs_output \
-    -j y \
-    -l h_rt=24:00:00,h_data=${data_per_core}G \
-    -pe shared ${cores} \
-    -M $USER@mail \
-    -m ea \
-    -hold_jid $hold_jid \
-    -terse \
-    -t 1-${total_samples}%8 \
-    $script_name "$origin" "$project_dir" "$cores")
-fi
-
-# return job ID to run_step.sh
-echo "$jobid"
+    -t 1-$total_samples:1%8 \
+    $script_name "$origin" "$project_dir" "$cores"
--- run_step.sh
+++ run_step.sh
@@ -2,35 +2,35 @@
 ###############################################################################
 #     Aydin Karatas
 #     Project Coprolite Viromes
-#     run_step.sh 
+#	  run_step.sh 
 ###############################################################################
 cd $HOME/project_coprolite_viromes
+
 
 script=$1
 filename=$(basename "$script")
 
 if [[ $filename =~ ^[0-9]+_ ]]; then
-    # initialize variable for dependency tracking
-    last_job_id=""
+    # run pal samples
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-AWC
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-BEL
+    ./run_ori.sh -s $script -d 8 -c 16 -p $SCRATCH/project_coprolite_viromes -o pal-BMS
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ENG
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ZAF
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pal-ZAP
 
-    # ordered origins
-    origins=( pal-AWC pal-BEL pal-BMS pal-ENG pal-ZAF pal-ZAP \
-            ind-DNK ind-ESP ind-USA \
-            pre-FJI pre-MDG pre-MEX pre-PER pre-TZA )
+    # run ind samples
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-DNK
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-ESP
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o ind-USA
 
-    for ori in "${origins[@]}"; do
-        if [[ -z "$last_job_id" ]]; then
-            echo "submitting $ori with no dependency"
-            jobid=$(./run_ori.sh -s "$script" -d 8 -c 8 -p "$SCRATCH/project_coprolite_viromes" -o "$ori")
-            echo "submitted $jobid for $ori with no dependency"
-        else
-            jobid=$(./run_ori.sh -s "$script" -d 8 -c 8 -p "$SCRATCH/project_coprolite_viromes" -o "$ori" -h "$last_job_id")
-            echo "submitted $jobid for $ori with dependency $jobid"
-        fi
-        last_job_id="$jobid"
-    done
-
+    # run non-ind samples
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-FJI
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-MDG
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-MEX
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-PER
+    ./run_ori.sh -s $script -d 8 -c 8 -p $SCRATCH/project_coprolite_viromes -o pre-TZA
 elif [[ $filename =~ ^[0-9]+a_ ]]; then
-    # run auxiliary script independently
+    # run auxilliar script that is not specific to an origin
     ./run_ori.sh -s $script -d 8 -c 4 -p $SCRATCH/project_coprolite_viromes -o all
 fi
